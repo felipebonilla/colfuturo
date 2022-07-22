3901af333cc285ec44460c704e9c3884233a6a78
felipebonilla <ybonilla@serempre.com>
2022-07-22 12:08:27 -0500

    initial commit

---
+++ apps/website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'colfuturo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'website';
+}
